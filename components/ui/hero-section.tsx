3a6937581e4028ae7c9a43b3fe7c3609087f80d3
Shaurya Gupta <ShauryaGupta118@gmail.com>
2025-07-11 02:19:40 +0530

    fix: adjust margin for container in HeroSection component

--- components/ui/hero-section.tsx
+++ components/ui/hero-section.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
           backgroundRepeat: 'no-repeat',
         }}
       >
-        <div className="container w-[100%] mx-auto relative">
+        <div className="container w-[100%] mx-auto -mt-12 relative">
           <div className="grid md:grid-cols-[70%_auto] gap-5">
             <div>
               <div className="xl:pt-[145px] xl:pb-[180px] lg:pt-[120px] lg:pb-[150px] md:pt-[90px] md:pb-[100px] sm:pt-[80px] sm:pb-[85px] py-[60px] xl:pl-10 pl-0">
